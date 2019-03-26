b0857ee21af40af67ad4c4b9ea739d08b69bc594
Matthew Abebe <matthewabebe.inbox@gmail.com>
2019-03-26 17:13:53 -0500

    dogYears

---
+++ main.js
@@ -0,0 +1,16 @@
+// This is my age. //
+const myAge = 25;
+// This is early years. //
+let earlyYears = 2;
+earlyYears *= 10.5;
+// This is later years. //
+let laterYears = myAge - 2;
+// This is later years multiplied by 4. //
+laterYears *= 4;
+console.log(earlyYears, laterYears);
+// This is my age in dog years. //
+myAgeInDogYears = earlyYears + laterYears;
+// This is my name with all lowercase letters. //
+myName = 'Matthew'.toLowerCase();
+// This states my name, human age, and dog age. //
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
